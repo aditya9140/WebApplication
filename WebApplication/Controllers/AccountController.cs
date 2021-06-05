5fccb081ceb24da8fcfbec28eb07c7e216692f1d
Aditya <adity@kumar.com>
2021-06-05 23:34:32 +0530

    Initial commit

---
+++ WebApplication/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using WebApplication.Models;
+namespace WebApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(Account account)
+        {
+            string connectionString = @"Data Source=DESKTOP-4DOQ385\SQLEXPRESS;Initial Catalog=DbTest;Integrated Security=True ";
+            using (SqlConnection con=new SqlConnection(connectionString))
+            {
+                con.Open();
+                string query = "insert into contact values(@Name,@Phone,@Email,@Text)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Name", account.Name);
+                cmd.Parameters.AddWithValue("@Phone", account.Phone);
+                cmd.Parameters.AddWithValue("@Email", account.Email);
+                cmd.Parameters.AddWithValue("@Text", account.Text);
+                cmd.ExecuteNonQuery();
+            }
+             return View("Create");
+        }
+
+
+    }
+}
---
+++ WebApplication/Models/Account.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Please Enter Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please Enter Phone No")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Email")]
+
+        public string Email { get; set; }
+        
+        public string Text { get; set; }
+    
+
+       }
+}
